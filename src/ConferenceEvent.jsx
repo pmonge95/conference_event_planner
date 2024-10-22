13810ff40208f5fd75d01475fc785ead1e88a02a
pmonge95 <pmonge95@gmail.com>
2024-10-22 12:03:42 -0400

    Added code for ad ons

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -7,6 +7,7 @@ const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -139,12 +140,23 @@ const ConferenceEvent = () => {
                         {/*Necessary Add-ons*/}
                         <div id="addons" className="venue_container container_main">
                             <div className="text">
-
                                 <h1> Add-ons Selection</h1>
-
                             </div>
                             <div className="addons_selection">
-
+                                {avItems.map((item, index) => (
+                                    <div className="av_data venue_main" key={index}>
+                                        <div className="img">
+                                            <img src={item.img} alt={item.name} />
+                                        </div>
+                                        <div className="text"> {item.name} </div>
+                                        <div> ${item.cost} </div>
+                                        <div className="addons_btn">
+                                            <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                            <span className="quantity-value">{item.quantity}</span>
+                                            <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                                        </div>
+                                    </div>
+                                ))}   
                             </div>
                             <div className="total_cost">Total Cost:</div>
 
